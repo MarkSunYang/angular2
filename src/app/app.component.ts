6c22473a606c0238705aaa6187b03651f757e54c
malik <1394768061@qq.com>
2018-01-08 18:39:54 +0800

    180118

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
+import { Person } from './person';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'app';
+  constructor(private http:Http){
+
+  }
+
+  ngOninit(){
+ this.getData();
+  }
+
+getData(){
+var headers=new Headers();
+headers.append('Content-Type', 'application/json');
+headers.append('Access-Control-Allow-Origin','*');
+headers.append('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+headers.append(  'Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+headers.append('Access-Control-Allow-Credentials', 'true' );
+
+  this.http.get('http://localhost:64257/api/values/getList')
+  .map(res => res.text())
+  .subscribe( 
+  data=>data, 
+  err=>console.log(err),
+  () => console.log('Random Quote Complete')
+);
+}
+
+// sendData(data){
+//   var headers = new Headers();
+//   headers.append('Content-Type', 'application/x-www-form-urlencoded');
+//   this.http.post('','',)
+// }
+
+
+  gender=['male','female','unknow']
+
+  person=new Person(1,'Malik',18,this.gender[0],"");
+
+  submitted=false;
+
+  newHero() {
+    this.getData();
+   
+  this.submitted=true;
+  this.person = new Person(0, '',0, this.gender[0],'');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {FormsModule} from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HttpModule } from '@angular/http';
+import 'rxjs/add/operator/map';
 import { AppComponent } from './app.component';
-
+import { Http } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 @NgModule({
+  
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+
+ }
